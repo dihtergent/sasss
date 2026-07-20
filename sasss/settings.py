34536bdbc38258a2c4f9a31761d30ae952c0a0ed
siwadol rukchart <hikiyasabuno@gmail.com>
2026-07-20 11:59:21 +0700

    Configure Neon PostgreSQL database connection

--- sasss/settings.py
+++ sasss/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,11 @@ WSGI_APPLICATION = 'sasss.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://neondb_owner:npg_HOLR7sEkPU3D@ep-empty-credit-az2kegaa-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
