9308bcc7fd2ca9d6779e1c32cf906778e37e7f27
siwadol rukchart <hikiyasabuno@gmail.com>
2026-07-20 11:15:14 +0700

    yeah

--- web/views.py
+++ web/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render,HttpResponse
 from django.http import HttpResponse
 # Create your views here.
 def index (request) :
+    import datetime
+    context = {
+        "title" : "my home page"
+    }
+
+    context{"date"} = datetime.date.today()
     return render(request,'index.html')
 def about (request) :
     return HttpResponse("about")
